7d16e8f0db9444218618ac4464b3fb48d22493b6
VicentBG <vbatallerg@gmail.com>
2019-09-03 16:55:41 +0200

    Añadido el html de Todos

--- src/Todos.svelte
+++ src/Todos.svelte
@@ -23,5 +23,30 @@
     ];
 </script>
 
-<h2>Svelte ToDo App</h2>
-<input type="text" class="todo-input" placeholder="Introduce nueva tarea..." bind:value={newTodoTitle} on:keydown={addTodo}>
+<div class="container">
+    <h2>Svelte ToDo App</h2>
+    <input type="text" class="todo-input" placeholder="Introduce nueva tarea..." bind:value={newTodoTitle} on:keydown={addTodo}>
+
+    {#each filteredTodos as todo}
+        <div class="todo-item">
+            <TodoItem {...todo} on:deleteTodo={handleDeleteTodo} on:toggleComplete={handleToggleComplete} />
+        </div>
+    {/each}
+
+    <div class="inner-container">
+        <div><label><input type="checkbox" class="inner-container-input" on:change={changeAllTodos}>Marcar Todos</label></div>
+        <div>{todosRemaining} tareas pendientes</div>
+    </div>
+
+    <div class="inner-container">
+        <div>
+            <button on:click={() => updateFilter('all')} class:active="{currentFilter === 'all'}">Todos</button>
+            <button on:click={() => updateFilter('active')} class:active="{currentFilter === 'active'}">Pendientes</button>
+            <button on:click={() => updateFilter('completed')} class:active="{currentFilter === 'completed'}">Completados</button>
+        </div>
+
+        <div>
+            <button on:click={clearCompleted}>Borrar Completados</button>
+        </div>
+    </div>
+</div>
